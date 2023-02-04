0b1a858ca678eb3d4ed8606c2986fb8ae69aa5ad
fufef <malenkiymayonezic@gmail.com>
2023-02-04 19:29:50 +0500

    added commands

--- src/main/scala/botLogic/MessageHandler.scala
+++ src/main/scala/botLogic/MessageHandler.scala
@@ -6,17 +6,41 @@ class MessageHandler {
   def createAnswer(message: String): Message = {
     message match {
       case "заказать коммишку" => Message("ну заказывайте")
-      case "оставить гневный отзыв" => Message("ваш запрос обрабатывается")
+      case "оставить гневный отзыв" |
+           "меня оставили на линии. что делать?" => Message("ваш запрос обрабатывается. оставайтесь, пожалуйста, на линии...")
+      case "оставить радостный отзыв" => Message("спасибо благодарим за вашу обратную связь")
       case "ваш паблик лучший" => Message("спасибо вы тоже ничего")
       case "посмотреть прайс" => Message("прайс в разработке")
       case "задать вопрос" => Message("фурш вас слушает")
       case "хочу анек" => Message("купил фуршет шляпу а она ему - я цилиндр")
-      case "позвать фурша" => Message("фурш спит. ожидайте................")
-      case "главное меню"|"меню"|"начать"|"старт" => Message("что вас интересует???",
+      case "отозвать фурша" => Message("фурш отозван. пусть спит дальше")
+      case "другие действия" => Message("что вы хотите сделать??",
         Some(Keyboard(false, false, List(
           List(
-          Button("primary", TextAction(label = "заказать коммишку", `type` = "text"))
-        ),
+            Button("secondary", TextAction(label = "позвать фурша", `type` = "text")),
+            Button("secondary", TextAction(label = "ваш паблик лучший", `type` = "text")),
+          ),
+          List(
+            Button("secondary", TextAction(label = "оставить гневный отзыв", `type` = "text")),
+            Button("secondary", TextAction(label = "оставить радостный отзыв", `type` = "text"))
+          ),
+          List(
+            Button("secondary", TextAction(label = "меня оставили на линии. что делать?", `type` = "text"))
+          ),
+          List(
+            Button("primary", TextAction(label = "назад", `type` = "text"))
+          )
+        ))))
+      case "позвать фурша" => Message("фурш спит. ожидайте................",
+        Some(Keyboard(one_time = true, inline = false, List(
+          List(
+            Button("secondary", TextAction(label = "отозвать фурша", `type` = "text"))
+          )))))
+      case "главное меню" | "меню" | "начать" | "старт" | "назад" => Message("что вас интересует???",
+        Some(Keyboard(false, false, List(
+          List(
+            Button("primary", TextAction(label = "заказать коммишку", `type` = "text"))
+          ),
           List(
             Button("secondary", TextAction(label = "посмотреть прайс", `type` = "text"))
           ),
@@ -25,6 +49,9 @@ class MessageHandler {
           ),
           List(
             Button("secondary", TextAction(label = "хочу анек", `type` = "text"))
+          ),
+          List(
+            Button("secondary", TextAction(label = "другие действия", `type` = "text"))
           )))))
       case _ => Message("фурш вас не понимать")
     }
